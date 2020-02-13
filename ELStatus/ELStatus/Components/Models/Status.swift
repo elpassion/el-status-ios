2b126f1b9b7e5128e42bf0cfd1936a02585e8645
Bartłomiej Guminiak <guminiak.bartlomiej@gmail.com>
2020-02-13 16:29:09 +0100

    Update model

--- ELStatus/ELStatus/Components/Models/Status.swift
+++ ELStatus/ELStatus/Components/Models/Status.swift
@@ -1,9 +1,4 @@
 struct Status: Codable {
-    let statusType: StatusType
+    let type: StatusType
     let user: User
-
-    enum CodingKeys: String, CodingKey {
-        case statusType = "status_type"
-        case user = "user"
-    }
 }
