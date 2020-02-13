8aedbbd90c38cf772e56267fc3e2797520119271
Bartłomiej Guminiak <guminiak.bartlomiej@gmail.com>
2020-02-13 14:10:41 +0100

    Update RequestApiType

--- ELStatus/ELStatus/Components/Networking/RequestApiType.swift
+++ ELStatus/ELStatus/Components/Networking/RequestApiType.swift
@@ -1,4 +1,9 @@
 protocol RequestApiType {
     var resourcePath: String { get }
+    var method: RequestMethod { get }
     var params: [String: String] { get }
+}
+
+enum RequestMethod: String {
+    case get = "GET"
 }
